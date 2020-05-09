88f189bb06582b5c07eff9511207b28fd355202f
373watohk <e185720@373watohk.local>
2020-05-09 16:52:42 +0900

    homemenu

--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -17,7 +17,7 @@ defmodule MyAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-
+    get "/home", HomeController, :index
     resources "/users", UserController
   end
 

729fc8b9ce4f43a619e788f617b80d77ad8ddc1c
島袋太智 <e185712@e185712.local>
2020-05-12 23:11:01 +0900

    HTMLの記述

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.scss"
-import "../css/sign_up.scss"
+import "../css/header.scss"
+import "../css/button.scss"
+
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".

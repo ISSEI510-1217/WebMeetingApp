780322134e7042282535550fbef27fbae3246b9c
shinya yoshitaka <ushimiyay20@gmail.com>
2020-05-12 22:35:37 +0900

    エンドポイント設定

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :my_app, MyAppWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
+  # url: [host: "example.com", port: 80],
+  # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info

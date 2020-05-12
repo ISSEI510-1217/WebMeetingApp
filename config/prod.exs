16e769df23c22d1443d771f41c351c34b248f338
shinya yoshitaka <ushimiyay20@gmail.com>
2020-05-13 04:18:26 +0900

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :my_app, MyAppWeb.Endpoint,
     # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :info,
 
 # ## SSL Support
 #

b21131174dde6ce5ce7fa1e17c18320ac7892e5b
shinya yoshitaka <ushimiyay20@gmail.com>
2020-05-12 20:50:04 +0900

    add setting prod

--- config/prod.exs
+++ config/prod.exs
@@ -61,3 +61,14 @@ config :my_app, MyApp.Repo,
   database: "",
   ssl: true,
   pool_size: 2
+
+config :my_app, MyAppWeb.Endpoint,
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :my_app, MyApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2

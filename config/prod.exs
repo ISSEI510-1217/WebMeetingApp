786f1ea87574e1fd371d56a16428e0344b91e1f9
shinya yoshitaka <ushimiyay20@gmail.com>
2020-05-13 10:56:22 +0900

    ホスト変更

--- config/prod.exs
+++ config/prod.exs
@@ -39,7 +39,7 @@ config :logger, level: :info
 # `:compatible` for wider support.
 #
 # `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
+# and cert in disk or a relativpath inside priv, for example
 # "priv/ssl/server.key". For all supported SSL configuration
 # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
 #
@@ -59,7 +59,7 @@ config :my_app, MyApp.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "26270d53-41c1-43c9-b22c-13b5b6330579",
   password: "pw-af4d084e-3e95-4576-9d04-b086eb3a69b2",
-  host: "35.238.156.108",
+  host: "db",
   ssl: true,
   port: 5432,
   pool_size: 25

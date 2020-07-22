89d041be5fabd3a5177bbde00f064834a83f166d
shinya-tan <ushimiyay20@gmail.com>
2020-07-22 13:58:30 +0900

    リンクの追加

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :my_app, MyAppWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "serpentine-live-poodle.gigalixirapp.com", port: 80],
+  http: [port: 4000],  #<== 追加
+  check_origin: ["//web-meet-with1.gigalixirapp.com/"],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 # Do not print debug messages in production
 config :logger, level: :info
